7afd8b28f83b2162df060548272f9ae983a62d20
Cuong Ba Ngoc <ngoccuong0491@gmail.com>
2016-07-26 23:00:59 +0700

    add signup form

---
+++ apps/controllers/admin.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var router = express.Router();
+
+var user_md = require("../models/user");
+
+router.get("/", function(req, res){
+    res.json({"message": "This is Admin Page"});
+});
+
+router.get("/signup", function(req, res){
+    res.render("signup", {data: {}});
+});
+
+router.post("/signup", function(req, res){
+    var user = req.body;
+
+    if(user.email.trim().length == 0){
+        res.render("signup", {data: {error: "Email is required"}});
+    }
+
+    if(user.passwd != user.repasswd && user.passwd.trim().length != 0){
+        res.render("signup", {data: {error: "Password is not Match"}});
+    }
+
+    // Insert to DB
+    user = {
+        email: user.email,
+        password: user.passwd,
+        first_name: user.firstname,
+        last_name: user.lastname
+    };
+
+    var result = user_md.addUser(user);
+
+    if(!result){
+        res.render("signup", {data: {error: "Could not insert user data to DB"}});
+    }else{
+        res.json({message: "Insert success"});
+    }
+
+
+
+});
+
+module.exports = router;
