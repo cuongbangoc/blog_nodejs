d29272bbc1e2955ca8a0cfcab4686b6db7118632
Cuong Ba Ngoc <ngoccuong0491@gmail.com>
2016-08-07 23:46:44 +0700

    add detail post

--- apps/controllers/blog.js
+++ apps/controllers/blog.js
@@ -26,4 +26,25 @@ router.get("/", function(req, res){
     // res.render("blog/index");
 });
 
+router.get("/post/:id", function(req, res){
+    var data = post_md.getPostByID(req.params.id);
+
+    data.then(function(posts){
+        var post = posts[0];
+
+        var result = {
+            post: post,
+            error: false
+        };
+
+        res.render("blog/post", {data: result});
+    }).catch(function(err){
+        var result = {
+            error: "Could not get post detail"
+        };
+
+        res.render("blog/post", {data: result});
+    });
+});
+
 module.exports = router;
