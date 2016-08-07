4f0a10afa7ab9a1563bc67fd82eefd9228363386
Cuong Ba Ngoc <ngoccuong0491@gmail.com>
2016-08-07 23:18:34 +0700

    Add blog home page

--- apps/controllers/blog.js
+++ apps/controllers/blog.js
@@ -1,8 +1,29 @@
 var express = require("express");
 var router = express.Router();
 
+var post_md = require("../models/post");
+
 router.get("/", function(req, res){
-    res.json({"message": "This is Blog Page"});
+    // res.json({"message": "This is Blog Page"});
+
+    var data = post_md.getAllPosts();
+
+    data.then(function(posts){
+        var result = {
+            posts: posts,
+            error: false
+        };
+
+        res.render("blog/index", {data: result});
+    }).catch(function(err){
+        var result = {
+            error: "Could not get posts data"
+        };
+
+        res.render("blog/index", {data: result});
+    });
+
+    // res.render("blog/index");
 });
 
 module.exports = router;
